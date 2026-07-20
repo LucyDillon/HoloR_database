310e5dab5f61d98a0585eb4d7c75fbd63020c3aa
Lucy Dillon <62658632+LucyDillon@users.noreply.github.com>
2026-07-20 14:55:17 +0100

    Update gtdbtk.sh

--- gtdbtk.sh
+++ gtdbtk.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-#SBATCH --time=13-23:59:59
-#SBATCH --partition=k2-lowpri
-#SBATCH --mem=150GB
+#SBATCH --time=23-23:59:59
+#SBATCH --partition=k2-lowpri,k2-bioinf
+#SBATCH --mem=100GB
 #SBATCH --ntasks=20
-#SBATCH --job-name=gtdbtk
-#SBATCH --error=gtdbtk-%A-%a.err
+#SBATCH --job-name=gtdbtk_all
+#SBATCH --mail-user=l.dillon@qub.ac.uk
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --error=gtdbtk_all-%A-%a.err
 
-module load apps/anaconda3/2024.10/bin
+
 source activate /mnt/scratch2/igfs-anaconda/conda-envs/gtdbtk-2.1.1
 
-gtdbtk classify_wf --genome_dir bins/ --out_dir gtdbtk_results/ --extension fa --cpus 20  --skip_ani_screen
+gtdbtk classify_wf --genome_dir skder_output_100/Dereplicated_Representative_Genomes --out_dir dereplicated_results/gtdbtk_results/ --extension fasta --cpus 20  --skip_ani_screen
+
