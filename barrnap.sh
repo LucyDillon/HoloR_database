70f9e5240cb29da9d0ec99de558b9b53fa65283e
Lucy Dillon <62658632+LucyDillon@users.noreply.github.com>
2026-07-20 14:55:58 +0100

    Modify SLURM settings and enhance barrnap script
    Updated SLURM job parameters and added checks for output files. Modified the source activation path for the conda environment.

--- barrnap.sh
+++ barrnap.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 #SBATCH --time=7-23:59:59
-#SBATCH --partition=k2-lowpri
+#SBATCH --partition=k2-lowpri,k2-bioinf
+#SBATCH --mail-user=ldillon05@qub.ac.uk
 #SBATCH --mem=50G
-#SBATCH --error=barrnap-%A-%a.err
-#SBATCH --job-name=barrnap
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --error=barrnap_mising-%A-%a.err
+#SBATCH --job-name=barrnap_missing
 
-source activate barrnap
+source activate /mnt/scratch2/igfs-anaconda/conda-envs/barrnap_0.9
 
-for i in *fa; do barrnap --kingdom bac --outseq ${i%.fasta}.barrnap.gff $i; done
+
+for i in $(cat genomes_skder_final_without_extension.txt); do
+    bac_out="dereplicated_results/barrnap/${i}.16S.fa"
+    arc_out="dereplicated_results/barrnap/${i}.arc.16S.fa"
+    if [ ! -s "$bac_out" ]; then
+
+        barrnap --kingdom bac --outseq "$bac_out" fasta_files_for_drep/${i}.fasta
+
+    fi
+
+    if [ ! -s "$arc_out" ]; then
+
+        barrnap --kingdom arc --outseq "$arc_out" fasta_files_for_drep/${i}.fasta
+
+    fi
+
+done
